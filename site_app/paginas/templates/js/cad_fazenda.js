b92f44617f8bd9c7f6a0d300e42498c2c2022168
Josué Nunes Campos <josue.campos@ufv.br>
2021-12-13 09:04:11 -0300

    Update cadastro assíncrono

--- site_app/paginas/templates/js/cad_fazenda.js
+++ site_app/paginas/templates/js/cad_fazenda.js
@@ -1,35 +1,35 @@
 var camposValidosFazenda = false;
 
-$("#salvar").click(function(event){
+$("#salvar").click(function(event) {
     event.preventDefault();
     validaCamposFazenda();
-    if(camposValidosFazenda){
+    if (camposValidosFazenda) {
         $(".loader").toggle();
         $("#form_fazenda").submit();
     }
 });
 
-function validaCamposFazenda(){
+function validaCamposFazenda() {
     var nomeFazenda = $("#nome_fazenda").val();
     var localizacaoFazenda = $("#localizacao_fazenda").val();
     var criacaoExtensiva = $('#criacao_extensiva').is(':checked');
     var criacaoIntensiva = $('#criacao_intensiva').is(':checked');
-    if(nomeFazenda != "" && localizacaoFazenda != "" && (criacaoExtensiva || criacaoIntensiva)){
+    if (nomeFazenda != "" && localizacaoFazenda != "" && (criacaoExtensiva || criacaoIntensiva)) {
         camposValidosFazenda = true;
-    }else{
+    } else {
         alert("Campo(s) vazio(s) detectado(s)! Por favor, preencha todos os campos.");
     }
 }
 
-$("#voltar").click(function(event){
+$("#voltar").click(function(event) {
     event.preventDefault();
     location.href = "/";
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
     $('#form_fazenda').submit(function(event) {
-        var formData = new FormData($(this)[0]); 
-        $.ajax({ 
+        var formData = new FormData($(this)[0]);
+        $.ajax({
             type: $(this).attr('method'),
             url: $(this).attr('action'),
             data: formData,
@@ -37,34 +37,32 @@ $(document).ready(function(){
             contentType: false,
             enctype: 'multipart/form-data',
             processData: false,
+            async: true,
 
-            success: function(result){
+            success: function(result) {
                 $(".loader").toggle();
                 result = JSON.parse(result);
-                if(result.resposta == 'OK'){
+                if (result.resposta == 'OK') {
                     alert("Fazenda cadastrada com sucesso!");
                     location.href = "/";
-                }else if(result.resposta == "FAZENDA EXISTENTE"){
+                } else if (result.resposta == "FAZENDA EXISTENTE") {
                     alert("Fazenda já cadastrada!");
                     $("nome_fazenda").val("");
                     $("localizacao_fazenda").val("");
-                }else{
+                } else {
                     alert("Erro interno! Tente novamente mais tarde.");
                 }
             },
-            fail: function(msg){
+            fail: function(msg) {
                 $(".loader").toggle();
                 alert("Erro ao salvar!");
             },
-            beforeSend: function(){
-            },
-            complete: function(msg){
-            },
-            error: function(msg){
+            beforeSend: function() {},
+            complete: function(msg) {},
+            error: function(msg) {
                 $(".loader").toggle();
                 alert("Erro interno!");
-            },
-            timeout: 180000
+            }
         });
         event.preventDefault();
     });
